01747863df524868025a50c0bb2745f64285004e
Ryoma SHINYA <sinya8282@gmail.com>
2011-06-20 10:16:37 +0900

    add parallel matching code generation.

--- src/paralleldfa.h
+++ src/paralleldfa.h
@@ -2,9 +2,6 @@
 #define  REGEN_PARALLEL_DFA_H_
 #include "util.h"
 #include "dfa.h"
-#include <boost/thread.hpp>
-#include <boost/bind.hpp>
-#include <boost/function.hpp>
 
 namespace regen {
 
@@ -12,6 +9,7 @@ class ParallelDFA: public DFA {
 public:
   ParallelDFA(const DFA &dfa, std::size_t thread_num = 2);
   typedef std::map<int, int> ParallelTransition;
+  bool Compile();
   bool FullMatch(const std::string &str) const { return FullMatch((unsigned char*)str.c_str(), (unsigned char *)str.c_str()+str.length()); }
   bool FullMatch(const unsigned char *str, const unsigned char *end) const;
   struct TaskArg {
@@ -21,11 +19,13 @@ public:
   };
 private:
   void FullMatchTask(TaskArg targ) const;
+  int *transition__;
   mutable std::vector<int> parallel_states_;
   std::size_t dfa_size_;
   std::size_t thread_num_;
   std::deque<bool> dfa_accepts_;
   std::vector<ParallelTransition> parallel_transitions_;
+  int (*CompiledFullMatchTask)(const unsigned char *, const unsigned char *);
 };
 
 } // namespace regen
--- src/test.cc
+++ src/test.cc
@@ -1,15 +1,18 @@
 #include "regex.h"
 #include "dfa.h"
 #include "paralleldfa.h"
-#include <boost/timer.hpp>
 
 int main(int argc, char *argv[]) {
   std::string regex;
   int opt;
   int thread_num = 2;
+  bool compile = false;
 
-  while ((opt = getopt(argc, argv, "f:t:")) != -1) {
+  while ((opt = getopt(argc, argv, "cf:t:")) != -1) {
     switch(opt) {
+      case 'c':
+        compile = true;
+        break;
       case 'f': {
         std::ifstream ifs(optarg);
         ifs >> regex;
@@ -34,14 +37,14 @@ int main(int argc, char *argv[]) {
 
   const regen::DFA &dfa = r.dfa();
   regen::ParallelDFA pdfa = regen::ParallelDFA(dfa, thread_num);
+  if (compile) pdfa.Compile();
+  //regen::ParallelDFA pdfa = regen::ParallelDFA(dfa, thread_num);
 
   std::string text;
-  boost::timer t;
   regen::Util::mmap_t mm(argv[optind]);
 
-  t.restart();
   bool r1 = pdfa.FullMatch(mm.ptr, mm.ptr+mm.size);
-  printf("PDFA: %d(%.3f)\n", r1, t.elapsed());
+  printf("PDFA: %d\n", r1);
   
   return 0;
 }
